7d7d95a37412e6762b0307b57c9f6a8458f5e193
brlyman <lyman.brad3211@gmail.com>
2018-06-06 10:25:37 -0700

    Create Some Linear Algebra Helpers

---
+++ lib/linalg.ex
@@ -0,0 +1,18 @@
+defmodule Linalg do
+  def dot(x, y) when is_list(x) and is_list(y) and length(x) == length(y) do
+    Enum.zip(x, y)
+    |> Enum.map(fn {a, b} -> a*b end)
+    |> Enum.reduce(0, fn(x, acc) -> x + acc end)
+  end
+
+  def pretty(vec) when is_list(vec) do
+    vec
+    |> Enum.map(&Linalg.pretty/1)
+    |> Enum.join(", ")
+    |> (fn str -> "[" <> str <> "]" end).()
+  end
+
+  def pretty(num) when is_number(num) do
+    Float.round(num, 3) |> Float.to_string
+  end
+end
