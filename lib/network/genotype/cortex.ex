ebe44ca0adea2260f49dde7c8323be5fbcc1f4e2
brlyman <lyman.brad3211@gmail.com>
2018-06-09 14:52:05 -0700

    Add Docs For The Cortex

--- lib/network/genotype/cortex.ex
+++ lib/network/genotype/cortex.ex
@@ -1,6 +1,20 @@
 defmodule Genotype.Cortex do
   @moduledoc """
   The Cortex is responsible for synchronizing a network's input and output.
+
+  ## Examples
+
+    iex> cortex = Genotype.Cortex.new
+    iex> cortex = Genotype.Cortex.with_sensor cortex, "sensor"
+    iex> cortex = Genotype.Cortex.with_actuator cortex, "actuator"
+    iex> cortex = Genotype.Cortex.with_neuron cortex, "neuron"
+    iex> cortex.sensors
+    ["sensor"]
+    iex> cortex.actuators
+    ["actuator"]
+    iex> cortex.neurons
+    ["neuron"]
+
   """
 
   import Genotype
@@ -8,6 +22,9 @@ defmodule Genotype.Cortex do
   @enforce [:id, :actuators, :sensors, :neurons]
   defstruct [:id, :actuators, :sensors, :neurons]
 
+  @doc """
+  Create a new Cortex with no actuators, sensors, or neurons.
+  """
   def new do
     %Genotype.Cortex{
       id: Genotype.unique_id(__MODULE__),
@@ -17,18 +34,27 @@ defmodule Genotype.Cortex do
     }
   end
 
+  @doc """
+  Add a sensor to the cortex.
+  """
   def with_sensor(%Genotype.Cortex{} = cortex, sensor)
     when is_binary(sensor)
   do
     Map.update! cortex, :sensors, &(&1 ++ [sensor])
   end
 
+  @doc """
+  Add a neuron to the cortex.
+  """
   def with_neuron(%Genotype.Cortex{} = cortex, neuron)
     when is_binary(neuron)
   do
     Map.update! cortex, :neurons, &(&1 ++ [neuron])
   end
 
+  @doc """
+  Add an actuator to the cortex.
+  """
   def with_actuator(%Genotype.Cortex{} = cortex, actuator)
     when is_binary(actuator)
   do
